330e65786a353f8af4dbaccb56992bce1574034f
xenerr <xenerr@me.com>
2016-09-04 18:56:25 +0200

    adding Image.jsx file

--- components/Index.jsx
+++ components/Index.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Image from './Image.jsx';
 
 export default class Index extends Component {
   render() {
     return (
-      <h1>Hello World</h1> 
+      <h1>Hello World</h1>
     )
   }
-} 
+}
+
 
 ReactDOM.render(<Index/>, document.querySelector('main'));
---
+++ src/js/entry.js
@@ -0,0 +1,9 @@
+// Import CSS
+import '../css/master.scss';
+
+// Import React and JS
+import HelloBox from './HelloBox';
+import React from 'react';
+
+// Render!
+React.render(<HelloBox />, document.getElementsByTagName('body')[0]);
