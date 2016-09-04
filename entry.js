d41850e0fda20fd9994fdc5d8458e6f4d4373e91
xenerr <xenerr@me.com>
2016-09-04 10:29:30 +0200

    pushing latest

--- entry.js
+++ entry.js
@@ -4,8 +4,11 @@ var Router = require('react-router')
 var Routes = require('./Routes.jsx')
 
 module.exports = function render(locals, callback) {
-  Router.run(Routes, locals.path, function (Handler) {
+  Router.render(Routes, locals.path, function (Handler) {
     var html = React.renderToStaticMarkup(React.createElement(Handler, locals))
     callback(null, '<!DOCTYPE html>' + html)
   })
 }
+
+
+
